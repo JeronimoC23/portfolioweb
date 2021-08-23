71d632da6300dacaf2271484640af788bf80c730
JeronimoC23 <jeronimoclinaz@gmail.com>
2021-08-23 14:49:44 -0300

    Agregando rutas en

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('home/', views.index, name="home"),
+    path('en/', views.indexen, name="eng"),
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render (request,'mainapp/index.html')
+
+
+def indexen(request):
+    return render (request,'mainapp/index_en.html')
